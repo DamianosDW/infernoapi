371486eec4402ea3e297f461a4a78c029461afb8
DamianosDW <damianosdw@gmail.com>
2019-04-13 00:51:10 +0200

    Add another get methods to UsersActivityController

--- src/main/java/ovh/damianosdw/infernoapi/endpoints/usersactivity/UsersActivityController.java
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/usersactivity/UsersActivityController.java
@@ -12,25 +12,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/infernoapi")
+@RequestMapping("/infernoapi/usersactivity")
 @AllArgsConstructor
 public class UsersActivityController
 {
     private final UserActivityRepository userActivityRepository;
 
-    @GetMapping("/usersactivity")
+    @GetMapping("")
     public List<UserActivity> getAll()
     {
         return userActivityRepository.findAll();
     }
 
-    @GetMapping("/usersactivity/{userId}")
-    public List<UserActivity> getUserActivityByUserId(@PathVariable("userId") int id)
+    @GetMapping("/{month}/usersActivity")
+    public List<UserActivity> getUsersActivityByMonth(@PathVariable("month") int month)
     {
-        return userActivityRepository.findAll().stream().filter(userId -> userId.getUserId() == id).collect(Collectors.toList());
+        return userActivityRepository.findAll().stream().filter(userActivity -> userActivity.getActivityDate().getMonthValue() == month).collect(Collectors.toList());
     }
 
-    @PutMapping("/usersactivity")
+    @GetMapping("/{userId}/{month}/userActivity")
+    public List<UserActivity> getUserActivityByMonth(@PathVariable("userId") int userId, @PathVariable("month") int month)
+    {
+        return userActivityRepository.findAll().stream().filter(userActivity -> userActivity.getUserId() == userId && userActivity.getActivityDate().getMonthValue() == month).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{userId}/userActivity")
+    public List<UserActivity> getUserActivityByUserId(@PathVariable("userId") int id)
+    {
+        return userActivityRepository.findAll().stream().filter(userActivity -> userActivity.getUserId() == id).collect(Collectors.toList());
+    }
+
+    @PutMapping("/send")
     public void sendUserActivity(@RequestBody UserActivity userActivity)
     {
         userActivityRepository.save(userActivity);
