78e27003fb75d8ac69e3b2e68a0eb740ce719552
DamianosDW <damianosdw@gmail.com>
2019-04-25 19:20:33 +0200

    Add new methods to VipActivityController (REST Controller)

--- src/main/java/ovh/damianosdw/infernoapi/endpoints/vipactivity/VipActivityController.java
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/vipactivity/VipActivityController.java
@@ -7,6 +7,7 @@ package ovh.damianosdw.infernoapi.endpoints.vipactivity;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ovh.damianosdw.infernoapi.dbmodels.CandidatesVipActivityCheck;
 import ovh.damianosdw.infernoapi.dbmodels.VipActivityCheck;
 import ovh.damianosdw.infernoapi.utils.ChannelsActivityUtils;
 
@@ -18,28 +19,40 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/infernoapi")
+@RequestMapping("/infernoapi/vipactivity")
 @AllArgsConstructor
 public class VipActivityController
 {
     private final VipActivityModuleRepository vipActivityModuleRepository;
     private final VipActivityCheckRepository vipActivityCheckRepository;
+    private final CandidatesVipActivityCheckRepository candidatesVipActivityCheckRepository;
 
     // vip_activity_module
-    @GetMapping("/vipactivity/numberofchannels")
+    @GetMapping("/numberofchannels")
     public int getNumberOfChannels()
     {
         return vipActivityModuleRepository.findAll().get(0).getNumberOfChannels();
     }
 
-    @GetMapping("/vipactivity/channelsinuse")
+    @GetMapping("/channelsinuse")
     public int getChannelsInUse()
     {
         return vipActivityModuleRepository.findAll().get(0).getChannelsInUse();
     }
 
-    // vip_activity_check
-    @GetMapping("/vipactivity/monthly/{channelNumber}/{month}")
+    @PutMapping("/channelsinuse/update/{channelsInUse}")
+    public void updateChannelsInUse(@PathVariable("channelsInUse") int channelsInUse)
+    {
+        vipActivityModuleRepository.updateChannelsInUse(channelsInUse);
+    }
+    @PutMapping("/numberofchannels/update/{numberOfChannels}")
+    public void updateNumberOfChannels(@PathVariable("numberOfChannels") int numberOfChannels)
+    {
+        vipActivityModuleRepository.updateNumberOfChannels(numberOfChannels);
+    }
+
+    // vip_activity_check (admins)
+    @GetMapping("/monthly/{channelNumber}/{month}/vipactivity")
     public Map<String, Integer> getMonthlyVipActivity(@PathVariable("channelNumber") int channelNumber, @PathVariable("month") int month)
     {
         HashMap<String, Integer> allVipActivity = new HashMap<>();
@@ -68,7 +81,7 @@ public class VipActivityController
         return allVipActivity;
     }
 
-    @GetMapping("/vipactivity/weekly")
+    @GetMapping("/weekly/vipactivity")
     public Map<Integer, Integer> getAllWeeklyVipActivity()
     {
         int channelsInUse = vipActivityModuleRepository.findAll().get(0).getChannelsInUse();
@@ -98,7 +111,8 @@ public class VipActivityController
         }
         return allVipActivity;
     }
-    @GetMapping("/vipactivity/weekly/{channelNumber}")
+
+    @GetMapping("/weekly/{channelNumber}/vipactivity")
     public int getWeeklyVipActivity(@PathVariable("channelNumber") int channelNumber)
     {
         int channelVipActivity = 0;
@@ -119,5 +133,101 @@ public class VipActivityController
         }
 
         return channelVipActivity;
+    }
+
+    @PutMapping("/send")
+    public void sendVipActivity(@RequestBody VipActivityCheck vipActivityCheck)
+    {
+        vipActivityCheckRepository.save(vipActivityCheck);
+    }
+
+    // candidates_vip_activity_check
+    @GetMapping("/monthly/{channelNumber}/{month}/candidates/vipactivity")
+    public Map<String, Integer> getMonthlyCandidatesVipActivity(@PathVariable("channelNumber") int channelNumber, @PathVariable("month") int month)
+    {
+        HashMap<String, Integer> allVipActivity = new HashMap<>();
+        List<CandidatesVipActivityCheck> monthlyActivity = candidatesVipActivityCheckRepository.findAll()
+                .stream()
+                .filter(vipActivity -> vipActivity.getCheckDate().getMonthValue() == month && vipActivity.getCheckDate().getYear() == LocalDateTime.now().getYear())
+                .collect(Collectors.toList());
+
+        // Get max channel activity
+        String activityDate = monthlyActivity.get(0).getCheckDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        int maxActivity = 0;
+
+        for (CandidatesVipActivityCheck vipActivity : monthlyActivity)
+        {
+            // Save max activity, update activity date and reset max activity
+            if(!activityDate.equals(vipActivity.getCheckDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))))
+            {
+                allVipActivity.put(activityDate, maxActivity);
+                activityDate = vipActivity.getCheckDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                maxActivity = 0;
+            }
+
+            if(ChannelsActivityUtils.getCandidatesVipChannelActivity(channelNumber, vipActivity) > maxActivity)
+                maxActivity = ChannelsActivityUtils.getCandidatesVipChannelActivity(channelNumber, vipActivity);
+        }
+        return allVipActivity;
+    }
+
+    @GetMapping("/weekly/candidates/vipactivity")
+    public Map<Integer, Integer> getAllWeeklyCandidatesVipActivity()
+    {
+        int channelsInUse = vipActivityModuleRepository.findAll().get(0).getChannelsInUse();
+
+        // Prepare HashMap
+        HashMap<Integer, Integer> allVipActivity = new HashMap<>();
+        for(int i = 1; i <= channelsInUse; i++)
+            allVipActivity.put(i, 0);
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        List<VipActivityCheck> weeklyVipActivity = vipActivityCheckRepository.findAll()
+                .stream()
+                .filter(vipActivity -> vipActivity.getCheckDate().isAfter(currentDateTime.minusWeeks(1).minusDays(1)))
+                .collect(Collectors.toList());
+
+        // Get vip channels activity
+        for (VipActivityCheck vipActivity : weeklyVipActivity)
+        {
+            for(int i = 1; i <= channelsInUse; i++)
+            {
+                if( ChannelsActivityUtils.getVipChannelActivity(i, vipActivity) > allVipActivity.get(i))
+                {
+                    allVipActivity.put(i, ChannelsActivityUtils.getVipChannelActivity(i, vipActivity));
+                }
+            }
+        }
+        return allVipActivity;
+    }
+
+    @GetMapping("/weekly/{channelNumber}/candidates/vipactivity")
+    public int getWeeklyCandidatesVipActivity(@PathVariable("channelNumber") int channelNumber)
+    {
+        int channelVipActivity = 0;
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        List<CandidatesVipActivityCheck> weeklyVipActivity = candidatesVipActivityCheckRepository.findAll()
+                .stream()
+                .filter(vipActivity -> vipActivity.getCheckDate().isAfter(currentDateTime.minusWeeks(1).minusDays(1)))
+                .collect(Collectors.toList());
+
+        // Get vip channel activity
+        for (CandidatesVipActivityCheck vipActivity : weeklyVipActivity)
+        {
+            if(ChannelsActivityUtils.getCandidatesVipChannelActivity(channelNumber, vipActivity) > channelVipActivity)
+            {
+                channelVipActivity = ChannelsActivityUtils.getCandidatesVipChannelActivity(channelNumber, vipActivity);
+            }
+        }
+
+        return channelVipActivity;
+    }
+
+    @PutMapping("/candidates/send")
+    public void sendCandidatesVipActivity(@RequestBody CandidatesVipActivityCheck candidatesVipActivityCheck)
+    {
+        candidatesVipActivityCheckRepository.save(candidatesVipActivityCheck);
     }
 }
--- src/main/java/ovh/damianosdw/infernoapi/utils/ChannelsActivityUtils.java
+++ src/main/java/ovh/damianosdw/infernoapi/utils/ChannelsActivityUtils.java
@@ -5,6 +5,7 @@
 
 package ovh.damianosdw.infernoapi.utils;
 
+import ovh.damianosdw.infernoapi.dbmodels.CandidatesVipActivityCheck;
 import ovh.damianosdw.infernoapi.dbmodels.SpActivityCheck;
 import ovh.damianosdw.infernoapi.dbmodels.VipActivityCheck;
 
@@ -119,6 +120,74 @@ public abstract class ChannelsActivityUtils
         }
     }
     public static int getVipChannelActivity(int channelNumber, VipActivityCheck vipActivity)
+    {
+        switch(channelNumber)
+        {
+            case 1:
+                return vipActivity.getVip1();
+            case 2:
+                return vipActivity.getVip2();
+            case 3:
+                return vipActivity.getVip3();
+            case 4:
+                return vipActivity.getVip4();
+            case 5:
+                return vipActivity.getVip5();
+            case 6:
+                return vipActivity.getVip6();
+            case 7:
+                return vipActivity.getVip7();
+            case 8:
+                return vipActivity.getVip8();
+            case 9:
+                return vipActivity.getVip9();
+            case 10:
+                return vipActivity.getVip10();
+            case 11:
+                return vipActivity.getVip11();
+            case 12:
+                return vipActivity.getVip12();
+            case 13:
+                return vipActivity.getVip13();
+            case 14:
+                return vipActivity.getVip14();
+            case 15:
+                return vipActivity.getVip15();
+            case 16:
+                return vipActivity.getVip16();
+            case 17:
+                return vipActivity.getVip17();
+            case 18:
+                return vipActivity.getVip18();
+            case 19:
+                return vipActivity.getVip19();
+            case 20:
+                return vipActivity.getVip20();
+            case 21:
+                return vipActivity.getVip21();
+            case 22:
+                return vipActivity.getVip22();
+            case 23:
+                return vipActivity.getVip23();
+            case 24:
+                return vipActivity.getVip24();
+            case 25:
+                return vipActivity.getVip25();
+            case 26:
+                return vipActivity.getVip26();
+            case 27:
+                return vipActivity.getVip27();
+            case 28:
+                return vipActivity.getVip28();
+            case 29:
+                return vipActivity.getVip29();
+            case 30:
+                return vipActivity.getVip30();
+            default:
+                return 0;
+        }
+    }
+    public static int getCandidatesVipChannelActivity(int channelNumber, CandidatesVipActivityCheck vipActivity)
     {
         switch(channelNumber)
         {
