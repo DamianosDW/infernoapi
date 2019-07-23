6d90614d471c2c891689bacd553a817561dc0bbd
DamianosDW <damianosdw@gmail.com>
2019-07-24 01:55:43 +0200

    Create database table model - error_reports_messages

---
+++ src/main/java/ovh/damianosdw/infernoapi/dbmodels/ErrorReportMessage.java
@@ -0,0 +1,37 @@
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernoapi.dbmodels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "error_reports_messages")
+public class ErrorReportMessage
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MESSAGE_ID")
+    private int messageId;
+    @Column(name = "REPORT_ID", nullable = false)
+    private int reportId;
+    @Column(name = "USER_ID", nullable = false)
+    private int userId;
+    @Basic
+    @Column(name = "message_sent_date", nullable = false)
+    private LocalDateTime messageSentDate;
+    @Column(name = "message", nullable = false)
+    @Type(type="text")
+    private String message;
+}
