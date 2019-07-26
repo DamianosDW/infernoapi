2bdcbccf1278fa8e4b03d812c05b7b33d40c52ba
DamianosDW <damianosdw@gmail.com>
2019-07-27 01:05:20 +0200

    Add method to TeamMeetingsVotesRepository

--- src/main/java/ovh/damianosdw/infernoapi/endpoints/teammeetingsvotes/TeamMeetingsVotesRepository.java
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/teammeetingsvotes/TeamMeetingsVotesRepository.java
@@ -3,12 +3,14 @@
  * https://damianosdw.ovh
  */
 
-package ovh.damianosdw.infernoapi.endpoints.teammeetingreports;
+package ovh.damianosdw.infernoapi.endpoints.teammeetingsvotes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ovh.damianosdw.infernoapi.dbmodels.TeamMeetingVote;
 
+import java.util.List;
+
 public interface TeamMeetingsVotesRepository extends JpaRepository<TeamMeetingVote, Integer>, TeamMeetingsVotesCustom
 {
-
+    List<TeamMeetingVote> getTeamMeetingVotesByUserId(int userId);
 }
