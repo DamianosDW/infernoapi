44332140cf84536a3cb534fdfa6de289c7fa4b7c
DamianosDW <damianosdw@gmail.com>
2019-07-27 00:57:51 +0200

    Use custom repository in main interface and create new method

--- src/main/java/ovh/damianosdw/infernoapi/endpoints/teammeetingsmodule/TeamMeetingsModuleRepository.java
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/teammeetingsmodule/TeamMeetingsModuleRepository.java
@@ -3,16 +3,23 @@
  * https://damianosdw.ovh
  */
 
-package ovh.damianosdw.infernoapi.endpoints.teammeetingreports;
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernoapi.endpoints.teammeetingsmodule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ovh.damianosdw.infernoapi.dbmodels.TeamMeetingsModule;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public interface TeamMeetingsModuleRepository extends JpaRepository<TeamMeetingsModule, Integer>
+public interface TeamMeetingsModuleRepository extends JpaRepository<TeamMeetingsModule, Integer>, TeamMeetingsModuleCustom
 {
     TeamMeetingsModule getTeamMeetingsModuleByTeamMeetingDate(LocalDateTime teamMeetingDate);
     TeamMeetingsModule getTeamMeetingsModuleByDateConfirmedIsTrueAndTeamMeetingDateBetween(LocalDateTime currentDateTime, LocalDateTime currentDateTimePlusYear);
+    List<TeamMeetingsModule> getTeamMeetingsModulesByTeamMeetingDateBetweenOrderByTeamMeetingDateAsc(LocalDateTime currentDateTime, LocalDateTime currentDateTimePlusYear);
 
 }
