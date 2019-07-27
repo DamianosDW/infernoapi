d48b1a96fda6dc7fd2f14d101b337a230e7c0929
DamianosDW <damianosdw@gmail.com>
2019-07-27 21:56:39 +0200

    Add another method to WorkEvaluationRepository

--- src/main/java/ovh/damianosdw/infernoapi/endpoints/workevaluation/WorkEvaluationRepository.java
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/workevaluation/WorkEvaluationRepository.java
@@ -8,7 +8,10 @@ package ovh.damianosdw.infernoapi.endpoints.workevaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ovh.damianosdw.infernoapi.dbmodels.WorkEvaluation;
 
+import java.util.List;
+
 public interface WorkEvaluationRepository extends JpaRepository<WorkEvaluation, Integer>, WorkEvaluationCustom
 {
     int countWorkEvaluationsByUserId(int userId);
+    List<WorkEvaluation> getWorkEvaluationsByUserIdOrderBySentDateDesc(int userId);
 }
