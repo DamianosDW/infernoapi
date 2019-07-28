f96d16866d5a56c119960ac77f369b3c3b0f2b41
DamianosDW <damianosdw@gmail.com>
2019-07-29 01:43:37 +0200

    Create jpa repository for db table model - users

---
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/users/UsersRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernoapi.endpoints.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Integer>, UsersCustom
+{
+    User getUserByUsernameAndActiveIsTrue(String username);
+}
