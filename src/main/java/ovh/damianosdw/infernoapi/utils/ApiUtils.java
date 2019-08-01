832360294161292ede63eeb81a667308bd6d7194
DamianosDW <damianosdw@gmail.com>
2019-08-01 02:00:31 +0200

    Add another variables to ApiUtils

--- src/main/java/ovh/damianosdw/infernoapi/utils/ApiUtils.java
+++ src/main/java/ovh/damianosdw/infernoapi/utils/ApiUtils.java
@@ -5,9 +5,15 @@
 
 package ovh.damianosdw.infernoapi.utils;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public abstract class ApiUtils
 {
+    private static PasswordEncoder clientPasswordEncoder = new BCryptPasswordEncoder(4);
     public static final String DEFAULT_AVATAR_URL = "https://damianosdw.ovh/icp/images/logo-its3.png";
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = clientPasswordEncoder.encode("");
 
     public static boolean checkIfValueIsCorrect(Object object)
     {
