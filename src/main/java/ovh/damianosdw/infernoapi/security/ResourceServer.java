1065bd8213acafc95630808d2c89eb24aad8ce1e
DamianosDW <damianosdw@gmail.com>
2019-08-01 02:12:33 +0200

    Enable resource server and configure it

---
+++ src/main/java/ovh/damianosdw/infernoapi/security/ResourceServer.java
@@ -0,0 +1,36 @@
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernoapi.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+@Configuration
+@EnableResourceServer
+@AllArgsConstructor
+public class ResourceServer extends ResourceServerConfigurerAdapter
+{
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception
+    {
+        resources.resourceId("rest");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        http.anonymous().disable()
+                .requestMatchers().antMatchers("/infernoapi/**")
+                .and().authorizeRequests()
+                .antMatchers("/infernoapi/**").access("hasRole('ADMIN') or hasRole('USER')")
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+}
