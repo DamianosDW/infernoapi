89a6295dc615c88ec82d94cc17f5dd1207bf3bc0
DamianosDW <damianosdw@gmail.com>
2019-08-22 18:17:09 +0200

    Prepare custom handlers

---
+++ src/main/java/ovh/damianosdw/infernoapi/security/FailureHandler.java
@@ -0,0 +1,23 @@
+/*
+ * Created by DamianosDW
+ * https://damianosdw.ovh
+ */
+
+package ovh.damianosdw.infernoapi.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FailureHandler extends SimpleUrlAuthenticationFailureHandler
+{
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException
+    {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
