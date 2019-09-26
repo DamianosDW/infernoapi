1950048d0d61ca205b5683209bea13a6aa1618f9
DamianosDW <damianosdw@gmail.com>
2019-09-27 01:26:03 +0200

    Create necessary constructors for UserInfo

--- src/main/java/ovh/damianosdw/infernoapi/endpoints/users/UserInfo.java
+++ src/main/java/ovh/damianosdw/infernoapi/endpoints/users/UserInfo.java
@@ -5,13 +5,25 @@
 
 package ovh.damianosdw.infernoapi.endpoints.users;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfo
 {
     private int userId;
     private String username;
     private String position;
     private String avatarUrl;
+
+    public UserInfo(int userId, String username, String position)
+    {
+        this.userId = userId;
+        this.username = username;
+        this.position = position;
+        this.avatarUrl = "-";
+    }
 }
