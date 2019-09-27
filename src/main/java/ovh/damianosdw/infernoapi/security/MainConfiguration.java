b9ee2967f17db2ef0e804fc6eaac78c78a3d856b
DamianosDW <damianosdw@gmail.com>
2019-09-27 15:49:48 +0200

    Fix userDetailsService() bean - use JdbcUserDetailsManager

--- src/main/java/ovh/damianosdw/infernoapi/security/MainConfiguration.java
+++ src/main/java/ovh/damianosdw/infernoapi/security/MainConfiguration.java
@@ -8,13 +8,12 @@ package ovh.damianosdw.infernoapi.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -39,13 +38,11 @@ public class MainConfiguration
     @Bean
     public UserDetailsService userDetailsService(final PasswordEncoder userPasswordEncoder)
     {
-        return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("")
-                        .password(userPasswordEncoder.encode(""))
-                        .authorities("read", "write")
-                        .build()
-        );
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user, password, active FROM users WHERE user = ?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user, role FROM users WHERE user = ?");
+        return jdbcUserDetailsManager;
     }
 
     @Bean
